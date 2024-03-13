10531896f7bebbf43cc79ca17fb9007dc6ab47cf
rajmishra-47 <rajnandanmishra2002@gmail.com>
2024-03-13 18:44:00 +0530

    chnaged CSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-// import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
@@ -11,16 +10,11 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
 import { Component3Component } from './component-3/component-3.component';
-// import { CommonModule } from '@angular/common';
-// import { RouterOutlet } from '@angular/router';
 import Chart from 'chart.js/auto';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { AnyCatcher } from 'rxjs/internal/AnyCatcher';
-// import * as _ from 'lodash';
-
 import { FormControl, Validators } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
-
 import { ComponentDropDownComponent } from './component-drop-down/component-drop-down.component';
 import { SharedService } from './shared.service';
 import { Subscription } from 'rxjs';
@@ -48,11 +42,11 @@ export class AppComponent {
 
   Wells: any;
   WellsName: any;
-  WellsVal: any[] = [];
+  WellsVal: any;
 
   Res: any;
   ResName: any;
-  ResVal: any;
+  ResVal: any=[];
 
   TotalName: any ;
   TotalVal: any;
@@ -81,6 +75,9 @@ export class AppComponent {
 
   Total:any
 
+
+  resname=['Reserviour-1','Reserviour-2']
+
   onRowClicked(dummy: any) {}
 
   displayedColumns: string[] = ['lable', 'num'];
@@ -100,12 +97,17 @@ export class AppComponent {
       // this.createChart1(this.WellsName,this.WellsVal);
       // this.createChart3(this.WellsName,this.WellsVal);
 
+
+
     });
     
     this.http.get('http://localhost:8000/showRes').subscribe((data: any) => {
       this.Res = data;
       this.ResName = this.Res.map((data: any) => data.resName);
       this.ResVal = this.Res.map((data: any) => Number(data.resval));
+
+      console.log(this.ResName);
+      
 
       this.createChart3(this.ResName, this.ResVal);
 
@@ -115,9 +117,11 @@ export class AppComponent {
 
       this.s1=100
 
-      this.Total=[ {name:'Site-1',Val:this.s1},
 
-                    {name:"Site-2",Val:this.s2}]
+
+      this.Total=[ {name:'Storage Site-1',Val:this.s1},
+
+                    {name:"Storage site-2",Val:this.s2}]
 
 
 
@@ -150,7 +154,7 @@ export class AppComponent {
             },
           ],
         },
-        // options: { aspectRatio:2},
+      
       });
     }
   }
@@ -175,7 +179,7 @@ export class AppComponent {
             },
           ],
         },
-        // options: { aspectRatio: 2.5 },
+      
       });
     }
   }
@@ -198,36 +202,87 @@ export class AppComponent {
             },
           ],
         },
-        // options: { aspectRatio: 2.0 },
       });
     }
   }
 
   editButtonClicked1() {
     this.Wflag = !this.Wflag;
+    this.editflag1 = !this.editflag1;
+    this.chartDisabled = !this.chartDisabled;
 
     if (this.Wflag) {
       this.Wb1 = 'Save';
-    } else {
+    } 
+    
+    else
+     {
+
       this.Wb1 = 'Edit';
+      this.createChart2(this.WellsName,this.WellsVal)
+      this.s1=0
+
+    this.WellsVal.forEach((num:any)=> this.s1+=num)
+
+  
+    this.Total=[ {name:'Site-1',Val:this.s1},
+
+                  {name:"Site-2",Val:this.s2}]
+
+
+    this.TotalName=this.Total.map((data:any)=>data.name)
+
+    this.TotalVal=this.Total.map((data:any)=>data.Val)
+
+
+
+    this.createChart1(this.TotalName,this.TotalVal)
     }
 
-    this.editflag1 = !this.editflag1;
-    this.chartDisabled = !this.chartDisabled;
+      
+    
   }
 
   editButtonClicked2() {
+    
     this.Rflag = !this.Rflag;
+    this.editflag2 = !this.editflag2;
+    this.chartDisabled = !this.chartDisabled;
 
     if (this.Rflag) {
       this.Rb1 = 'Save';
-    } else {
-      this.Rb1 = 'Edit';
-    }
+     
+    } 
+    
+      else 
+    {
+      this.Rb1 = 'Edit'
+      this.createChart3(this.ResName,this.ResVal)
 
-    this.editflag2 = !this.editflag2;
-    this.chartDisabled = !this.chartDisabled;
+      this.s2=0
+
+
+    this.ResVal.forEach((num:any)=> this.s2+=num)
+
+
+    this.Total=[ {name:'Site-1',Val:this.s1},
+
+                  {name:"Site-2",Val:this.s2}]
+
+
+    this.TotalName=this.Total.map((data:any)=>data.name)
+
+    this.TotalVal=this.Total.map((data:any)=>data.Val)
+
+    this.createChart1(this.TotalName,this.TotalVal)
+    
   }
+  
+}
+
+
+
+
 
   applyChange() {
     this.WellsVal = this.Wells.map((data: any) => Number(data.wval));
@@ -235,8 +290,6 @@ export class AppComponent {
     this.ResVal = this.Res.map((data: any) => Number(data.resval));
 
     this.createChart2(this.WellsName, this.WellsVal);
-
-    console.log(this.Wells, this.WellsVal);
 
     this.createChart3(this.ResName, this.ResVal);
 
@@ -246,7 +299,6 @@ export class AppComponent {
 
     this.ResVal.forEach((num:any)=> this.s2+=num)
 
-    console.log(this.s2);
 
     this.Total=[ {name:'Site-1',Val:this.s1},
 
@@ -259,8 +311,29 @@ export class AppComponent {
 
     this.createChart1(this.TotalName,this.TotalVal)
 
-    console.log(this.TotalVal);
+ 
+  }
 
+
+  checkInputValue1(value: any, index: any) {
+    const parsedValue = parseInt(value);
+    if (parsedValue < 0) {
+      // Assuming you want to reset to 1 if the input is negative
+      this.WellsVal[index] = 1;
+    } else {
+      this.WellsVal[index]= parsedValue;
+    }
+  }
+
+
+  checkInputValue2(value: any, index: number) {
+    const parsedValue = parseInt(value);
+    if (parsedValue < 0) {
+      // Assuming you want to reset to 1 if the input is negative
+      this.ResVal[index]= 1;
+    } else {
+      this.ResVal[index] = parsedValue;
+    }
   }
 
 }
